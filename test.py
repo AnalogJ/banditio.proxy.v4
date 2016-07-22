f41bda45282e2de4a78e10563acb48b6b97ca2c0
Jason Kulatunga <jk17@ualberta.ca>
2016-07-22 14:49:47 -0700

    update pip

--- test.py
+++ test.py
@@ -1,27 +1,33 @@
+from models.entry import Entry
 
 
-headers = [
-               {
-                   "name": "Host",
-                   "value": "www.cnn.com"
-               },
-               {
-                   "name": "User-Agent",
-                   "value": "curl/7.43.0"
-               },
-               {
-                   "name": "Accept",
-                   "value": "*/*"
-               },
-               {
-                   "name": "Proxy-Connection",
-                   "value": "Keep-Alive"
-               },
-               {
-                   "name": "accept-encoding",
-                   "value": "identity"
-               }
-           ]
+entry = Entry()
 
-
-print {header['name']: header['value'] for header in headers}
+entry.test = 'hello'
+print entry.test
+#
+# headers = [
+#                {
+#                    "name": "Host",
+#                    "value": "www.cnn.com"
+#                },
+#                {
+#                    "name": "User-Agent",
+#                    "value": "curl/7.43.0"
+#                },
+#                {
+#                    "name": "Accept",
+#                    "value": "*/*"
+#                },
+#                {
+#                    "name": "Proxy-Connection",
+#                    "value": "Keep-Alive"
+#                },
+#                {
+#                    "name": "accept-encoding",
+#                    "value": "identity"
+#                }
+#            ]
+#
+#
+# print {header['name']: header['value'] for header in headers}
